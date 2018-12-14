01a5c61c7c2f562ab69cdfee61c99d30cc41d231
Jimmy Pendry <jimmy.pendry@gmail.com>
2018-12-14 15:50:56 -0500

    Improve output formatting

--- foobar.rb
+++ foobar.rb
@@ -1,58 +1,46 @@
 # frozen_string_literal: true
 require 'dotenv/load'
-require 'csv'
 require 'pry'
 require 'rest-client'
 require 'base64'
 require 'json'
 
-def encode_credentials(user_name, password)
-  "Basic #{::Base64.strict_encode64("#{user_name}:#{password}")}"
-end
+API_BASE_URI = 'https://www.logcheckapp.com/api/v1'
 
-def process(uuid)
-  api_base_uri = 'https://www.logcheckapp.com/api/v1/'
-  records_path = "logs/#{uuid}/records"
-  foobar = api_base_uri + records_path
+USER_NAME = ENV['TOKEN_ID']
+PASSWORD = ENV['SECRET_KEY']
 
-  user_name = ENV['TOKEN_ID']
-  password = ENV['SECRET_KEY']
-
-  response = RestClient.get foobar, {
-    params: { published_since: '2018-12-13T00:00:00Z' },
-    Authorization: encode_credentials(user_name, password)
-  }
-
-  print_response(response)
-rescue Exception => ex
-  puts ex.response
-end
-
-def print_response(response)
-  puts "user_recorded\t\t\tvalue"
-  puts "-------------\t\t\t-----"
-  
-  as_json = JSON.parse(response)
-  as_json['data'].each do |record|
-    attrs = record['attributes']
-
-    value = attrs['value']
-    recorded_at = Time.parse(attrs['user_recorded'])
-
-    puts "#{recorded_at.strftime('%D %r')}\t\t#{value}"
+def print_response(json_response)
+  json_response['data'].each do |record|
+    print_record(record)
   end
 end
 
-schema_file = 'SJC35_Rounds_Schema.txt'
-csv_options = {
-  col_sep: "\t",
-  headers: true,
-  header_converters: :symbol
-}
+def print_record(json_record)
+  attrs = json_record['attributes']
 
-counter = 0
-CSV.foreach(schema_file, csv_options) do |row|
-  process('6d7fa70f802f411a9ea208ed1d17ebf9') if counter == 0
-  counter += 1
+  value = attrs['value']
+  recorded_at = Time.parse(attrs['user_recorded'])
+
+  puts "#{recorded_at.strftime('%D %r')}\t\t#{value}"
 end
 
+# Setup
+log_uuid = '6d7fa70f802f411a9ea208ed1d17ebf9'
+records_path = "logs/#{log_uuid}/records"
+request_uri = "#{API_BASE_URI}/#{records_path}"
+
+# Make the request
+response = RestClient.get request_uri, {
+  params: { published_since: '2018-12-13T00:00:00Z' },
+  Authorization: "Basic #{::Base64.strict_encode64("#{USER_NAME}:#{PASSWORD}")}"
+}
+
+# Print the response
+puts '=========================================================================='
+puts "Readings for 'Electrical Room A (187) / SWA - Load (kVA)' since 2018-12-13"
+puts '=========================================================================='
+puts ''
+puts "user_recorded\t\t\tvalue"
+puts "-------------\t\t\t-----"
+print_response(JSON.parse(response))
